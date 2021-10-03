c59c950800e828c8083f55adf24e9318c89f4ab1
Oscar Pérez <mr.omiguelperez@gmail.com>
2021-10-03 17:36:41 -0500

    fix deploy

--- .github/scripts/copy-ssh-keys.sh
+++ .github/scripts/copy-ssh-keys.sh
@@ -2,5 +2,5 @@
 
 
 mkdir -p ~/.ssh/
-cp -rv ./ansible/**/files/ssh_keys/instance/academusoftv2_ansible_aws* ~/.ssh/
+cp -rv ./ansible/files/ssh_keys/instance/academusoftv2_ansible_aws* ~/.ssh/
 chmod 600 ~/.ssh/academusoftv2_ansible_aws*
