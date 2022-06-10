ed5a22b799786dc963262988ad7b43a43db00332
Alberto Perez <aapp1106@gmail.com>
2022-06-10 14:28:47 -0500

    feat: se consume el servicio de login

--- academusoft_wa/src/app/modules/login/login.component.ts
+++ academusoft_wa/src/app/modules/login/login.component.ts
@@ -2,17 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Apollo, gql, QueryRef } from 'apollo-angular';
 import { AcademusoftService } from 'src/app/core/services/academusoft.service';
-const GET_POSTS = gql`
-query {
-  allStudent {
-    id,
-    name,
-    username,
-    nuip
-    
-  }
-}
-`;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -27,36 +16,39 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   private querySubscription: any;
 
-  ngOnInit(): void {
-    
-    
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy() {
     this.querySubscription.unsubscribe();
   }
 
-  async func () {
-    let valor = await this.service.allStudent();
-    console.log(valor);
-  }
-
   formulario = new FormGroup({
-    Identificacion: new FormControl(null,[Validators.max(9999999999999),Validators.min(0),Validators.required]),
+    Identificacion: new FormControl(null,Validators.required),
     Pass: new FormControl(null,Validators.required),
   });
 
-  activacion = new FormGroup({
-    codigo: new FormControl(null,[Validators.required]),
-    correo: new FormControl(null,[Validators.required]),
-  });
-
+  token:string = '';
   async guardar(){
-    await this.func();
     if(this.formulario.invalid){
       alert("Invalido");
       return;
     }
-    alert("Valido");
+    const UPVOTE_POST = gql`mutation {
+      login(Auth: {username: "${this.formulario.value.Identificacion}", password: "${this.formulario.value.Pass}"}) {
+        token
+      }
+    }`;
+    this.apollo.mutate({
+      mutation: UPVOTE_POST,
+      variables: {
+        postId: 12
+      }
+    }).subscribe(({ data } : any) => {
+      this.token = data?.login.token
+      console.log(this.token);
+    },(error) => {
+      alert("Invalido");
+      console.log('there was an error sending the query', error);
+    });
   }
 }
